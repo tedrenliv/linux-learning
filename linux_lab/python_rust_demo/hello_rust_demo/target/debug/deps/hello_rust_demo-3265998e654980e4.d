437655fdbce91d4634a4192b623c5ec23fa3596d
John Wren <126983594+tedrenliv@users.noreply.github.com>
2026-02-19 16:13:44 +0000

    Add Python + Rust demo project

---
+++ linux_lab/python_rust_demo/hello_rust_demo/target/debug/deps/hello_rust_demo-3265998e654980e4.d
@@ -0,0 +1,5 @@
+/workspaces/linux-learning/linux_lab/python_rust_demo/hello_rust_demo/target/debug/deps/hello_rust_demo-3265998e654980e4.d: src/main.rs
+
+/workspaces/linux-learning/linux_lab/python_rust_demo/hello_rust_demo/target/debug/deps/hello_rust_demo-3265998e654980e4: src/main.rs
+
+src/main.rs:
---
+++ linux_lab/python_rust_demo/hello_rust_demo/target/debug/hello_rust_demo.d
@@ -0,0 +1,1 @@
+/workspaces/linux-learning/linux_lab/python_rust_demo/hello_rust_demo/target/debug/hello_rust_demo: /workspaces/linux-learning/linux_lab/python_rust_demo/hello_rust_demo/src/main.rs
